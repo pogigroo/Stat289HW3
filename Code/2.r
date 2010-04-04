994270013d3993f16c1c7925beb1ef511c1fce4a
Eric Groo <eric.groo@gmail.com>
2010-04-04 15:38:13 -0400

    Attempt at answer to Q2
    The body of the solution was taken from p. 135.
    
    The result of the grid approximation looks strange.

---
+++ Code/2.r
@@ -0,0 +1,23 @@
+data <- read.table("../SAT.dat",header = TRUE)
+
+logpost.mutau <- function(mu, tau, y=data$esteffect, sig=data$seeffect){
+	logprior <- -log(tau)/2
+	loglike  <- sum(log(dnorm(y, mean=mu, sd=sqrt(sig^2 + tau))))
+	logpost <- logprior + loglike
+	return(logpost)
+}
+
+mugrid <- ppoints(100)*20 # mu between 0 and 
+taugrid <- ppoints(100)*5 #per the hint
+
+logpost <- matrix(NA,100,100)
+for (i in 1:100){ 
+  for (j in 1:100){
+    logpost[i,j] <- logpost.mutau(mugrid[i],taugrid[j])
+  }
+}
+
+post <- exp(logpost-max(logpost))
+post <- post/sum(post)
+
+contour(mugrid,taugrid,post, drawlabels=FALSE)
