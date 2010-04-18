07d737844ed0f6b2bbb20b9d3c16f164adb83b15
Eric Groo <eric.groo@gmail.com>
2010-04-18 13:17:44 -0400

    Completing problem three and updating writeup.
    I just noticed that draws for tau are flattening out. I'm going to
    checkout the previous commits to looks for the same behavior.

--- Code/3.r
+++ Code/3.r
@@ -8,7 +8,7 @@ logpost.mutau <- function(mu, tau, y=data$esteffect, sig=data$seeffect){
 }
 
 mugrid <- ppoints(1000)*20-3
-taugrid <- ppoints(1000)*10 #per the hint
+taugrid <- ppoints(1000)*4 #smaller, to aid numerical integration
 
 logpost <- matrix(NA,1000,1000)
 for (i in 1:1000){ 
@@ -20,26 +20,34 @@ for (i in 1:1000){
 post <- exp(logpost-max(logpost))
 post <- post/sum(post)
 
+######################################################################
+# Numerical integration is easier than deriving the marginal 
+# conditionals
+######################################################################
 post.mu.weights <- apply(post,1,sum)
-post.tau.weights <- apply(post,2,sum)
 
-# plot(post.mu.weights, type = "l")
-# plot(post.tau.weights, type="l")
-# 
-# post.mu.draws <- sample(mugrid, size=1000, replace=T, prob=post.mu.weights)
-# post.tau.draws <- array(NA, 1000)
-# for (i in post.mu.draws) {
-# 	post.tau.draws[i] <- sample(taugrid, size=1, prob=post[mugrid==i, ])
-# }
-
-post.tau.draws <- sample(taugrid, size=1000, replace=T, prob=post.tau.weights)
-post.mu.draws <- array(NA, 1000)
-for (i in post.tau.draws) {
-	post.mu.draws[i] <- sample(mugrid, size=1, prob=post[,taugrid==i])
+post.mu.draws <- sample(mugrid, size=1000, replace=T, prob=post.mu.weights)
+post.tau.draws <- array(NA, 1000)
+for (i in post.mu.draws) {
+	post.tau.draws[i] <- sample(taugrid, size=1, prob=post[mugrid==i, ])
 }
 
-summary(post.mu.draws)
+mean(post.mu.draws)
+quantile(post.mu.draws,c(0.025,0.5,0.975))
+mean(post.tau.draws)
+quantile(post.tau.draws,c(0.025,0.5,0.975))
 summary(post.tau.draws)
+hist(post.tau.draws)
 
-# par(mar=c(3.5,3.5,2,1), mgp=c(2,.65,0), las=1, cex.main=1.5)
-# contour(mugrid,taugrid,post, ylim=c(0,0.6), drawlabels=FALSE, main= expression(paste("Marginal Posterior Distribution of ", plain(p)(mu,tau^2))))
+######################################################################
+# Numerically integrating over mu isn't as stable as the above
+######################################################################
+# post.tau.weights <- apply(post,2,sum)
+# post.tau.draws <- sample(taugrid, size=1000, replace=T, prob=post.tau.weights)
+# post.mu.draws <- array(NA, 1000)
+# for (i in post.tau.draws) {
+# 	post.mu.draws[i] <- sample(mugrid, size=1, prob=post[,taugrid==i])
+# }
+# 
+# summary(post.mu.draws)
+# summary(post.tau.draws)
